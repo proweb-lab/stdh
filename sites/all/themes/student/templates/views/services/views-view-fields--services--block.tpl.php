7e0f7f4fbe07eebc35b2e070720bc15ac571ef02
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-04-28 14:00:50 +0300

    service  view tpl

---
+++ sites/all/themes/student/templates/views/services/views-view-fields--services--block.tpl.php
@@ -0,0 +1,11 @@
+<div class="service">
+    <div class="service-image">
+        <?php print $fields['field_image']->content;?>
+    </div>
+    <div class="service-info">
+        <h3><?php print $fields['title']->content;?></h3>
+        <div class="service-description">
+            <?php print $fields['field_body']->content;?>
+        </div>  
+    </div>
+</div>
