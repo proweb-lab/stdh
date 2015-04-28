876de43a8f729b011e1fa904de1502073a41e815
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-04-28 13:00:00 +0300

    accordion

--- sites/all/themes/student/templates/fields/field--field_information.tpl.php
+++ sites/all/themes/student/templates/fields/field--field_information.tpl.php
@@ -5,17 +5,19 @@ if (count($variables['element']['#items']) > 0) {
     $items[$key] = $value['value'];
   }
   ?>
-
+<div id="accordion">
          <?php foreach ($items as $key => $value) { ?>
-           <?php $item = $variables['element'][$key]['entity']['field_collection_item'][$value]; ?>
-      <div class="slideshow-item" >
-        <?php echo render($item['field_title'][0]); ?>
-        <div class="slideshow-text cycle-overlay">
+        
+   <?php $item = $variables['element'][$key]['entity']['field_collection_item'][$value]; ?>
+    <h3><?php echo render($item['field_title'][0]); ?></h3>
+        <div class="colloection-info">
           <?php echo render($item['field_body'][0]); ?>
         </div>
        
-      </div>
+     
     <?php } ?>
+</div>
+    
  
  
 <?php } ?>
