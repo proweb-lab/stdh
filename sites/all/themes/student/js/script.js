1841c6d31c43428808d966bb6fa8747bafa0f1bf
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-04-28 13:03:05 +0300

    accordion

---
+++ sites/all/themes/student/js/script.js
@@ -0,0 +1,12 @@
+(function ($, Drupal, window, document, undefined) {
+  Drupal.behaviors.student = {
+    attach: function (context, settings) {
+      
+      $('#accordion').accordion();
+      
+      
+      
+      
+    }
+       };
+})(jQuery, Drupal, this, this.document);
