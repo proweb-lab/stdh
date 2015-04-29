2ed868ced5f38d9f81010c66210b4563ce5f0ed8
proweb-lab <mohammadgholeh@gmail.com>
2015-04-29 11:58:54 +0300

    offices front block

--- sites/all/themes/student/templates/page.tpl.php
+++ sites/all/themes/student/templates/page.tpl.php
@@ -62,7 +62,11 @@
       ?>
     </div>
     <hr>
+        <div class="offices-block">
+            <?php print views_embed_view('office','front_block')?>
+        </div>
     <?php }?>
+
     <div class="row">
 
     <?php if (!empty($page['sidebar_first'])): ?>
@@ -75,13 +79,8 @@
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php //if (!empty($title)): ?>
-        <!--<h1 class="page-header"><?php //print $title; ?></h1>-->
-      <?php //endif; ?>
-      <?php print render($title_suffix); ?>
+
       <?php print $messages; ?>
       <?php if (!empty($tabs)): ?>
         <?php print render($tabs); ?>
