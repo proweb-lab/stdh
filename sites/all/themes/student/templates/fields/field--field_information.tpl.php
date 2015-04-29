41ffe75f898f1795f8d2a262197c04c4aa2ba3ff
proweb-lab <mohammadgholeh@gmail.com>
2015-04-29 10:48:14 +0300

    javascript file for toggle

--- sites/all/themes/student/templates/fields/field--field_information.tpl.php
+++ sites/all/themes/student/templates/fields/field--field_information.tpl.php
@@ -5,11 +5,11 @@ if (count($variables['element']['#items']) > 0) {
     $items[$key] = $value['value'];
   }
   ?>
-<div id="accordion">
+<div class="accordion">
          <?php foreach ($items as $key => $value) { ?>
         
    <?php $item = $variables['element'][$key]['entity']['field_collection_item'][$value]; ?>
-    <h3><?php echo render($item['field_title'][0]); ?></h3>
+    <h3 class="collection-title"><?php echo render($item['field_title'][0]); ?></h3>
         <div class="colloection-info">
           <?php echo render($item['field_body'][0]); ?>
         </div>
