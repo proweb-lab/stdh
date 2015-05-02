b7ce8b9faddc414f1077735cb545936092c18816
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-02 12:31:05 +0300

    office page  tpl

--- sites/all/themes/student/js/script.js
+++ sites/all/themes/student/js/script.js
@@ -6,6 +6,20 @@
           $('.accordion .colloection-info').toggleClass('active');
           $('.accordion .colloection-info').slideToggle();
         });
+            $('.accordion .collection-title').click(function () {
+      if ($(this).parents('.colloction-info').hasClass('active')) {
+        $(this).parents('.colloction-info').removeClass('active');
+      }
+      else {
+        $(this).parents('.colloction-info').addClass('active');
+      }
+      $(this).parents('.colloction-info').find('.colloction-info-body').slideToggle('slow', function () {
+      });
+
+     
+
+
+    });
 
    
       
