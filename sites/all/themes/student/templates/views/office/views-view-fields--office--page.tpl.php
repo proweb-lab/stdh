28fb071c5341d9bd85470341651a9a5ec2cd7b73
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-02 12:08:52 +0300

    office page  tpl

---
+++ sites/all/themes/student/templates/views/office/views-view-fields--office--page.tpl.php
@@ -0,0 +1,14 @@
+<div id="office">
+    <div class="office-title">
+        <?php print $fields['title']->content; ?>
+    </div>
+    <div class="office-image">
+        <?php print $fields['field_image']->content; ?>
+    </div>
+    <div class="office-information">
+        <?php print $fields['field_information']->content; ?>
+    </div>
+    <div class="office-map">
+        <?php print $fields['field_map']->content; ?>
+    </div>
+</div>
