8c7413415ad6258d3cfd5d7dbe9904b4cda02e30
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-02 11:13:17 +0300

    office front block tpl

---
+++ sites/all/themes/student/templates/views/services/views-view-fields--services--front-block.tpl.php
@@ -0,0 +1,8 @@
+<div class="service-block">
+    <div class="service-image">
+        <?php print $fields['field_image']->content;?>
+    </div>
+    <div class="service-title">
+        <?php print $fields['title']->content;?>
+    </div>
+</div>>
