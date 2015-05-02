a1ff5f624f70f2d944a5f76d7fc994f83d0d3988
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-02 10:44:52 +0300

    office front block tpl

---
+++ sites/all/themes/student/templates/views/office/views-view-fields--office--front-block.tpl.php
@@ -0,0 +1,8 @@
+<div class="office-block">
+    <div class="office-title">
+        <?php print $fields['title']->content; ?>
+    </div>
+    <div class="office-image">
+        <?php print $fileds['field_image']->content;?>
+    </div>
+</div>
