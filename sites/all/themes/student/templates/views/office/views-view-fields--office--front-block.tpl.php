8f2de3efacc208d5540b77f555cb44164b449630
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-03 11:06:00 +0300

    menu style

--- sites/all/themes/student/templates/views/office/views-view-fields--office--front-block.tpl.php
+++ sites/all/themes/student/templates/views/office/views-view-fields--office--front-block.tpl.php
@@ -2,6 +2,7 @@
     <div class="office-title">
         <?php print $fields['title']->content; ?>
     </div>
+    <span class="extra-title"><?php print $fields['extra_title']->content; ?></span>
     <div class="office-image">
         <?php print $fields['field_image']->content;?>
     </div>
