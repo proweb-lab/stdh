ba645bb61f75f31fc56cc2238b4227fea0b86cda
proweb-lab <mohammadgholeh@gmail.com>
2015-05-04 14:52:51 +0300

    new buuton bottom up

--- sites/all/themes/student/js/script.js
+++ sites/all/themes/student/js/script.js
@@ -14,7 +14,12 @@
             $(this).slideToggle("fast");
         });
 
-      
+        if ( ($(window).height() + 100) < $(document).height() ) {
+            $('#top-link-block').removeClass('hidden').affix({
+                // how far to scroll down before link "slides" into view
+                offset: {top:100}
+            });
+        }
     }
        };
 })(jQuery, Drupal, this, this.document);
--- sites/all/themes/student/templates/page.tpl.php
+++ sites/all/themes/student/templates/page.tpl.php
@@ -132,10 +132,14 @@
             <?php print render($page['sidebar_second']); ?>
             </aside>  <!-- /#sidebar-second -->
             <?php endif; ?>
+        <span id="top-link-block" class="hidden">
+    <a href="#top" class="well well-sm"  onclick="$('html,body').animate({scrollTop:0},'slow');return false;">
+        <i class="glyphicon glyphicon-chevron-up"></i> Back to Top
+    </a>
+</span><!-- /top-link-block -->
 
     </div>
 </div>
-<a style="display: block;" href="#" class="scrollToTop button"><i class="enotype-icon-arrow-up"></i></a>
 <footer class="footer">
 <?php print render($page['footer']); ?>
 </footer>
