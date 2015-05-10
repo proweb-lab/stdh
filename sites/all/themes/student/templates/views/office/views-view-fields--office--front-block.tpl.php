349377f80e96779d043183f3a0be1fbeded5eeeb
proweb-lab <mohammadgholeh@gmail.com>
2015-05-10 15:26:13 +0300

    delete style span title in office block

--- sites/all/themes/student/templates/views/office/views-view-fields--office--front-block.tpl.php
+++ sites/all/themes/student/templates/views/office/views-view-fields--office--front-block.tpl.php
@@ -1,4 +1,6 @@
 <div class="office-block">
+
+    <span class="extra-title"><?php print $fields['field_extra_title']->content; ?></span>
     <div class="office-image">
         <?php print $fields['field_image']->content;?>
     </div>
