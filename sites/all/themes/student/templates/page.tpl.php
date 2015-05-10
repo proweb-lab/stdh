f33a2e150318d0e969eaf5cd78925ea7bbd7b554
proweb-lab <mohammadgholeh@gmail.com>
2015-05-10 15:08:26 +0300

    div offices-block container

--- sites/all/themes/student/templates/page.tpl.php
+++ sites/all/themes/student/templates/page.tpl.php
@@ -56,12 +56,12 @@
             ?>
         </div>
         <div class="block-universities">
-            <a class="btn btn-test-universities btn-lg">
+            <button class="btn btn-test-universities btn-lg">
                 <?php  $link_test = l(t('Test university orientation '),
                     'http://myqyas.monadsoft.com/startMyQyas.aspx',array('html' => TRUE, 'attributes' =>
                         array('target' => '_blank')));
                 echo $link_test;?>
-            </a>
+            </button>
             <button class="btn btn-universities btn-lg">
                 <?php  $link_universities = l(t('Search In Universities'), '/universities');
 
