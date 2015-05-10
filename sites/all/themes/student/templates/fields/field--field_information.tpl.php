70d39eb2ca0b14279adbca0307a21112f1d0c788
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-10 15:09:57 +0300

    office border

--- sites/all/themes/student/templates/fields/field--field_information.tpl.php
+++ sites/all/themes/student/templates/fields/field--field_information.tpl.php
@@ -9,13 +9,14 @@ if (count($variables['element']['#items']) > 0) {
          <?php foreach ($items as $key => $value) { ?>
         
    <?php $item = $variables['element'][$key]['entity']['field_collection_item'][$value]; ?>
+    <div class="office-info">
     <h3 class="collection-title"><?php echo render($item['field_title'][0]); ?></h3>
         <div class="colloction-info">
              <div class="colloction-info-body">
           <?php echo render($item['field_body'][0]); ?>
         </div>
         </div>
-       
+    </div>
      
     <?php } ?>
 </div>
