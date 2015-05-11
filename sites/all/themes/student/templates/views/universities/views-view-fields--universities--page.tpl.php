1c9ca0bcdfdad8f9dac078bc48a80ba6495a7271
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-11 15:12:21 +0300

    office border

--- sites/all/themes/student/templates/views/universities/views-view-fields--universities--page.tpl.php
+++ sites/all/themes/student/templates/views/universities/views-view-fields--universities--page.tpl.php
@@ -3,29 +3,18 @@
 <?php
 //$country = intval($node->field_recognition['und'][0]['value']);
 // switch($country){
-print '<pre>';
-print_r($fields['field_recognition']->content);
-print '</pre>';
-        if(($fields['field_recognition']->content) == 'Jordan'){
+
+        if(($fields['field_recognized_jo']->content) == 'Yes'){
           echo '<span class="reg-country">';
           print t('Jorn');
            print '</span>';
         }
-         if($fields['field_recognition']->content==2){
+         if($fields['field_recognized_qa']->content=='Yes'){
             echo '<span class="reg-country">';
           print t('Qater');
             print '</span>';
          }
-         if($fields['field_recognition']->content==3){
-            echo '<span class="reg-country">';
-         print t('UAE');
-           print '</span>';
-         }
-         if($fields['field_recognition']->content==4){
-            echo '<span class="reg-country">';
-            print t('kw');
-              print '</span>';
-         }
+       
      
  
        ?>
