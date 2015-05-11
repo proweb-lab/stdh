830e7b21d047ca3e817365aed55b50bf138df480
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-11 15:51:45 +0300

    office border

--- sites/all/themes/student/templates/views/universities/views-view-fields--universities--page.tpl.php
+++ sites/all/themes/student/templates/views/universities/views-view-fields--universities--page.tpl.php
@@ -8,10 +8,14 @@
 //print '</pre>';
 $field = field_info_field('field_recognized_jo');
 $allowed_values = list_allowed_values($field);
-print_r($allowed_values[2]);
+//print_r($allowed_values[2]);
         if(isset($allowed_values[1])){
           echo '<span class="reg-country">';
           print t('Jorn');
+           print '</span>';
+        }else{
+             echo '<span class="reg-country">';
+            print t('no');
            print '</span>';
         }
          if($fields['field_recognized_qa']->content=='Yes'){
