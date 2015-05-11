584533a4f3aa08de6c2228a74bb3f8886ca82243
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-11 16:17:22 +0300

    office border

--- sites/all/themes/student/templates/views/universities/views-view-fields--universities--page.tpl.php
+++ sites/all/themes/student/templates/views/universities/views-view-fields--universities--page.tpl.php
@@ -7,11 +7,11 @@
 //print_r($fields['field_recognized_jo']);
 //print '</pre>';
 $jo = field_info_field('field_recognized_jo');
-$allowed_values_jo = list_allowed_values($field);
+$allowed_values_jo = list_allowed_values($jo);
 //print_r($allowed_values[2]);
         if((isset($allowed_values_jo[1])){
             echo '<span class="reg-country">';
-          print t('Jordan yes');
+             print t('Jordan yes');
             print '</span>';
          }
          if(isset($allowed_values_jo[2])){
@@ -19,17 +19,17 @@ $allowed_values_jo = list_allowed_values($field);
             print t('no jordan');
            print '</span>';
         }
-         if($fields['field_recognized_qa']->content=='Yes'){
-            echo '<span class="reg-country">';
-          print t('yes Qer');
-            print '</span>';
-         } 
-         if($fields['field_recognized_qa']->content=='No'){
-             
-              echo '<span class="reg-country">';
-          print t('no qatar');
-            print '</span>';
-         }
+//         if($fields['field_recognized_qa']->content=='Yes'){
+//            echo '<span class="reg-country">';
+//          print t('yes Qer');
+//            print '</span>';
+//         } 
+//         if($fields['field_recognized_qa']->content=='No'){
+//             
+//              echo '<span class="reg-country">';
+//          print t('no qatar');
+//            print '</span>';
+//         }
        
    
  
