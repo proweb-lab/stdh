7c5c38193522027120386bf341cc9a619317f8ee
proweb-lab <mohammadgholeh@gmail.com>
2015-05-11 11:09:33 +0300

    add extra title office css

--- sites/all/themes/student/templates/views/office/views-view-fields--office--front-block.tpl.php
+++ sites/all/themes/student/templates/views/office/views-view-fields--office--front-block.tpl.php
@@ -4,12 +4,12 @@
    if(!empty( $extra_title)){
     ?>
 
-       <div class="ribbon-wrapper-green">
-           <div class="ribbon-green">NEWS</div>
+   <?php echo' <div class="ribbon-wrapper-green">
+           <div class="ribbon-green">NEWS</div>';
 
-           <?php print $extra_title->content;?>
-       </div>
-
+            print $extra_title->content;
+       echo '</div>';
+?>
     <?php }?>
     <div class="office-image">
         <?php print $fields['field_image']->content;?>
