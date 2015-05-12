fd4437821a73ce6391a7e1bfa9b06c941ace3ee9
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-12 15:57:13 +0300

    office border

--- sites/all/themes/student/templates/views/services/views-view-fields--services--block.tpl.php
+++ sites/all/themes/student/templates/views/services/views-view-fields--services--block.tpl.php
@@ -1,9 +1,10 @@
 <div class="service-block service-<?php print $fields['field_weight']->content;?>">
     <div class="service-title">
         <h3><?php print $fields['title']->content;?></h3>
-        <div class="service-description">
-            <?php print $fields['field_body']->content;?>
-        </div>  
     </div>
+    <div class="service-description">
+            <?php print $fields['field_body']->content;?>
+    </div>  
+    
 </div>
 
