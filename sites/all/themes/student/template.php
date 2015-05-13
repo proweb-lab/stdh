7a00a1ed1506eaf28cc7c16a28dacd037031f9ad
proweb-lab <mohammadgholeh@gmail.com>
2015-05-13 16:05:40 +0300

    add new template node 64

--- sites/all/themes/student/template.php
+++ sites/all/themes/student/template.php
@@ -10,7 +10,7 @@ function stdh_theme($existing, $type, $theme, $path){
 }
 function mytheme_preprocess_page(&$variables) {
     // For node templates.
-    $variables['node']->my_form = drupal_get_form('webform-client-form-64');
+    $variables['node/64']->my_form = drupal_get_form('webform-client-form-64');
     // For page templates.
     // $variables['my_form'] = drupal_get_form('form_name');
 }
