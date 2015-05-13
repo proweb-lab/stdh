96293d47547515eed494b7fd8073131a8d90c097
proweb-lab <mohammadgholeh@gmail.com>
2015-05-13 16:26:22 +0300

    add new template node 64

--- sites/all/themes/student/templates/node--64.tpl.php
+++ sites/all/themes/student/templates/node--64.tpl.php
@@ -1,9 +1,14 @@
 <h3 class="addresses"><?php print t('Addresses'); ?></h3>
 
 <?php
-$html = drupal_render(drupal_get_form('webform-client-form-64', $args));
+//$html = drupal_render(drupal_get_form('webform-client-form-64', $args));
 
 //$form = drupal_get_form('webform-client-form-64');
 //print drupal_render($form);
 ?>
 
+<?php
+$elements = drupal_get_form("webform-client-form-64");
+$form = drupal_render($elements);
+echo $form;
+?>
