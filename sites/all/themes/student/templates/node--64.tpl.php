aa5d1e30e21f60c6e5b42153a55d641cc8b43d33
proweb-lab <mohammadgholeh@gmail.com>
2015-05-13 15:31:18 +0300

    add new template node 64

--- sites/all/themes/student/templates/node--64.tpl.php
+++ sites/all/themes/student/templates/node--64.tpl.php
@@ -1,2 +1,2 @@
 <h3 class="addresses"><?php print t('addresses'); ?></h3>
-<p class="node-body"><?php print render($content['field_body']) ?></p>
+<p class="node-body"><?php print $fields['field_address']->content; ?></p>
