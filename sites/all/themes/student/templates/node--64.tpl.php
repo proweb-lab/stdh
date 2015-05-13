4ebcf2fb5eb9f85f0eafd443647b479f77a2e748
proweb-lab <mohammadgholeh@gmail.com>
2015-05-13 16:30:38 +0300

    add new template node 64

--- sites/all/themes/student/templates/node--64.tpl.php
+++ sites/all/themes/student/templates/node--64.tpl.php
@@ -8,7 +8,8 @@
 ?>
 
 <?php
-$elements = drupal_get_form("webform-client-form-64");
-$form = drupal_render($elements);
-echo $form;
+//$elements = drupal_get_form("webform-client-form-64");
+//$form = drupal_render($elements);
+//echo $form;
 ?>
+<?php print render(drupal_get_form('webform-client-form-64')); ?>
