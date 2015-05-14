f1afb10a553b2801283b608914c6b5ee98795f30
proweb-lab <mohammadgholeh@gmail.com>
2015-05-14 10:34:46 +0300

    add extra title office css

--- sites/all/themes/student/templates/node--64.tpl.php
+++ sites/all/themes/student/templates/node--64.tpl.php
@@ -1,12 +1,9 @@
-
-
-<h3 class="addresses"><?php print t('Addresses'); ?></h3>
 <?php
    $block_contact = module_invoke('webform', 'block_view', 'client-block-64');
    print render($block_contact['content']);
 ?>
 <div class="addresses">
-
+    <h3 class="addresses"><?php print t('Addresses'); ?></h3>
     <h4 class="addresses"><?php  print t("address");?></h4>
    <?php  print render($content['field_address']);?>
         <h4 class="phone"><?php print t("phone");?></h4>
