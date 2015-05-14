607c866543bfcf61a8590c39d30f478ecd364867
proweb-lab <mohammadgholeh@gmail.com>
2015-05-14 14:32:33 +0300

    add extra title office css

--- sites/all/themes/student/templates/node--64.tpl.php
+++ sites/all/themes/student/templates/node--64.tpl.php
@@ -8,8 +8,8 @@
     <div class="col-sm-4" >
         <div class="box">
         <div class="addresses_header"><h3><?php print t('Addresses information'); ?></h3></div>
-        <h4 class="phone_label"><span class="glyphicon glyphicon-phone"></span><?php print t("phone:").render($content['field_phone']);?></h4>
-        <h4 class="email_label"><span class="glyphicon glyphicon-envelope"></span><?php print t("email:").render($content['field_email']);?></h4>
+        <h4 class="phone_label"><?php print t("phone:").render($content['field_phone']);?></h4>
+        <h4 class="email_label"><?php print t("email:").render($content['field_email']);?></h4>
         </div>
     </div>
 
