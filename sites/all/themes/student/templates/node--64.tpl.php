973845ca1654d039d9c2679264148598e3c7f41c
proweb-lab <mohammadgholeh@gmail.com>
2015-05-14 13:45:33 +0300

    add extra title office css

--- sites/all/themes/student/templates/node--64.tpl.php
+++ sites/all/themes/student/templates/node--64.tpl.php
@@ -13,7 +13,19 @@
         <h4 class="email_label"><?php print t("email:").render($content['field_email']);?></h4>
             <h4 class="email_label"><?php print t("email:").render($content['field_branches']);?></h4>
 
+            <?php foreach ($items as $key => $value) { ?>
 
+                <?php $item = $variables['element'][$key]['entity']['field_collection_item'][$value]; ?>
+                <div class="office-info">
+                    <div class="collection-title"><?php echo render($item['field_title'][0]); ?></div>
+                    <div class="colloction-info">
+                        <div class="colloction-info-body">
+                            <?php echo render($item['field_body'][0]); ?>
+                        </div>
+                    </div>
+                </div>
+
+            <?php } ?>
 
         </div>
     </div>
