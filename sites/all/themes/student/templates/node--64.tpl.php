6cc0553faf43785f8d7c7e356b42d5dfb6687a6e
proweb-lab <mohammadgholeh@gmail.com>
2015-05-14 09:12:40 +0300

    add new template node 64

--- sites/all/themes/student/templates/node--64.tpl.php
+++ sites/all/themes/student/templates/node--64.tpl.php
@@ -5,7 +5,9 @@
    $block_contact = module_invoke('webform', 'block_view', 'client-block-64');
    print render($block_contact['content']);
 ?>
-
+<div class="addresses">
+    <?php print render($content['field_address']) ?>
+</div>
 
 
 
