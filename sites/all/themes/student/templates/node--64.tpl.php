409478c9792377da016194d4bee9cb247a6dafee
proweb-lab <mohammadgholeh@gmail.com>
2015-05-14 12:09:29 +0300

    add extra title office css

--- sites/all/themes/student/templates/node--64.tpl.php
+++ sites/all/themes/student/templates/node--64.tpl.php
@@ -8,7 +8,7 @@
     <div class="col-sm-2">
         <h3 class="addresses_header"><?php print t('Addresses'); ?></h3>
         <h4 class="addresses_label"><?php  print t("address:").render($content['field_address']);?></h4>
-        <h4 class="phone_label"><?php print t("phone:").render($content['field_phone']);?></h4>
+        <div class="phone_label"><?php print t("phone:").render($content['field_phone']);?></div>
         <h4 class="email_label"><?php print t("email:").render($content['field_email']);?></h4>
     </div>
 
