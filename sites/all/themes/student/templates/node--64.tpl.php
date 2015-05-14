242e7f4b8ed9f67a0f388dd48eed0c6244e7e455
proweb-lab <mohammadgholeh@gmail.com>
2015-05-14 13:48:29 +0300

    add extra title office css

--- sites/all/themes/student/templates/node--64.tpl.php
+++ sites/all/themes/student/templates/node--64.tpl.php
@@ -11,18 +11,19 @@
         <h4 class="addresses_label"><?php  print t("address:").render($content['field_address']);?></h4>
         <h4 class="phone_label"><?php print t("phone:").render($content['field_phone']);?></h4>
         <h4 class="email_label"><?php print t("email:").render($content['field_email']);?></h4>
+            <h4 class="email_label"><?php print t("email:").render($content['field_branches']);?></h4>
 
             <?php foreach ($items as $key => $value) { ?>
 
                 <?php $item = $variables['element'][$key]['entity']['field_collection_item'][$value]; ?>
-                <div class="office-info">
-                    <div class="collection-title"><?php echo render($item['field_title'][0]); ?></div>
-                    <div class="colloction-info">
-                        <div class="colloction-info-body">
-                            <?php echo render($item['field_body'][0]); ?>
-                        </div>
-                    </div>
-                </div>
+
+                  <?php echo render($item['field_country'][0]); ?>
+
+
+                            <?php echo render($item['field_countryphone'][0]); ?>
+
+                <?php echo render($item['field_email'][0]); ?>
+
 
             <?php } ?>
 
