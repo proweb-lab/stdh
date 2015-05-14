4442b2399c97fbb0353a8abaa026986783e673ca
proweb-lab <mohammadgholeh@gmail.com>
2015-05-14 09:03:28 +0300

    add new template node 64

--- sites/all/themes/student/templates/node--64.tpl.php
+++ sites/all/themes/student/templates/node--64.tpl.php
@@ -1,11 +1,11 @@
 <div class="row">
-    <div class="col-sm-6">
+
 <h3 class="addresses"><?php print t('Addresses'); ?></h3>
 <?php
    $block_contact = module_invoke('webform', 'block_view', 'client-block-64');
    print render($block_contact['content']);
 ?>
-    </div>
+
 
 
 
