cb1d0e8a159d6856daf9b256d70e22ce44c5a3ec
proweb-lab <mohammadgholeh@gmail.com>
2015-05-14 10:04:52 +0300

    add new template node 64

--- sites/all/themes/student/templates/node--64.tpl.php
+++ sites/all/themes/student/templates/node--64.tpl.php
@@ -6,7 +6,7 @@
    print render($block_contact['content']);
 ?>
 <div class="addresses">
-    <?php print rrender($item['field_title'][0]); ?>
+    <?php print rrender($item['field_title'][1]); ?>
 </div>
 
 
