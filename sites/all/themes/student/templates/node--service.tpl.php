a82e87cd30c7b333e22cf8e45e6d3e76a26de349
proweb-lab <mohammadgholeh@gmail.com>
2015-05-16 16:27:43 +0300

    create template node 69

--- sites/all/themes/student/templates/node--service.tpl.php
+++ sites/all/themes/student/templates/node--service.tpl.php
@@ -1,5 +1,5 @@
-
-    <h3 class="service-title">
+<div class="service-block service-<?php print $fields['field_weight']->content;?>">
+<h3 class="service-title">
         <?php print render($content['title']);?>
     </h3>
     <div class="service-description">
@@ -8,4 +8,4 @@
     <div class="service-price">
         <p><?php print  render($content['field_price']);?></p>
     </div>
-
+</div>
