01d81d3fcf7a43ebca09bc94bd02fbd28f83e849
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-16 12:09:28 +0300

    link module

--- sites/all/themes/student/templates/views/office/views-view-fields--office--front-block.tpl.php
+++ sites/all/themes/student/templates/views/office/views-view-fields--office--front-block.tpl.php
@@ -1,20 +1,20 @@
 <?php
+
 $html = '<div class="office-block">';
 
-   $extra_title = $fields['field_extra_title']->content;
-   if(!empty( $extra_title)){
+$extra_title = $fields['field_extra_title']->content;
+if (!empty($extra_title)) {
 
- $html .= '<div class="ribbon-wrapper-green">';
- $html .='<span>'. print t('soon') .'</span>';
- $html .='</div>';
-   }
-    $html .='<div class="office-image">';
-     $html .= $fields['field_image']->content;
+    $html .= '<div class="ribbon-wrapper-green">';
+    $html .='<span>' . print t('soon') . '</span>';
     $html .='</div>';
-    $html .='<h3 class="office-title">';
-    $html .= $fields['title']->content; 
-    $html .='</h3>';
-
+}
+$html .='<div class="office-image">';
+$html .= $fields['field_image']->content;
+$html .='</div>';
+$html .='<h3 class="office-title">';
+$html .= $fields['title']->content;
+$html .='</h3>';
 $html .= '</div>';
 
 
