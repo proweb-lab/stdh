65ad6276ed8c986cf9a6702930a24697faa1fb00
proweb-lab <mohammadgholeh@gmail.com>
2015-05-16 15:01:49 +0300

    create template node office 9

--- sites/all/themes/student/templates/node--office.tpl.php
+++ sites/all/themes/student/templates/node--office.tpl.php
@@ -1,3 +1,28 @@
-<div class="office-title">
-    <?php print render($title); ?>
-</div>
+<?php
+$items = array();
+if (count($variables['element']['#items']) > 0) {
+    foreach ($variables['element']['#items'] as $key => $value) {
+        $items[$key] = $value['value'];
+    }
+    ?>
+    <div class="accordion">
+        <?php foreach ($items as $key => $value) { ?>
+
+            <?php $item = $variables['element'][$key]['entity']['field_collection_item'][$value]; ?>
+            <div class="office-info">
+                <div class="collection-title"><?php echo render($item['field_title'][0]); ?></div>
+                <div class="colloction-info">
+                    <div class="colloction-info-body">
+                        <?php echo render($item['field_body'][0]); ?>
+                    </div>
+                </div>
+            </div>
+
+        <?php } ?>
+    </div>
+
+
+
+<?php } ?>
+
+<!--<span //class="glyphicon glyphicon-chevron-down"></span>-->
