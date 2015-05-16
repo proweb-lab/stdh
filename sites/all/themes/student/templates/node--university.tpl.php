b6de58c3f3a4b9015467191234d2c058b1411b1b
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-16 15:37:17 +0300

    link module

--- sites/all/themes/student/templates/node--university.tpl.php
+++ sites/all/themes/student/templates/node--university.tpl.php
@@ -17,6 +17,20 @@
      <h4><?php print t('KSA: ').render($content['field_recognized_ksa']);?></h4>
      <h4><?php print t('UAE: ').render($content['field_recognized_uae']);?></h4>
      <h4><?php print t('kwait: ').render($content['field_recognized_kw']);?></h4>
+     <table style="width:100%">
+  <tr>
+    <td><?php print t('Jordan')?></td>
+    <td><?php print t('KSA')?></td> 
+    <td><?php print t('UAE')?></td>
+    <td><?php print t('Kwait')?></td>
+  </tr>
+  <tr>
+    <td><?php print render($content['field_recognized_jo']);?></td>
+    <td><?php print render($content['field_recognized_ksa']);?></td> 
+    <td><?php print  render($content['field_recognized_uae']);?></td>
+     <td><?php print ender($content['field_recognized_kw']);?></td>
+  </tr>
+</table>
 </div>
 </div>
 
