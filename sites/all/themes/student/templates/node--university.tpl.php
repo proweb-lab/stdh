9270bb516a6d005f0d5277049155997b0ba78298
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-16 15:37:34 +0300

    link module

--- sites/all/themes/student/templates/node--university.tpl.php
+++ sites/all/themes/student/templates/node--university.tpl.php
@@ -13,10 +13,7 @@
 </div>
 <div class="uni-recognition">
      <h3><?php print t('Univrsity Recognition ')?></h3>
-    <h4><?php print t('Jordan: ').render($content['field_recognized_jo']);?></h4>
-     <h4><?php print t('KSA: ').render($content['field_recognized_ksa']);?></h4>
-     <h4><?php print t('UAE: ').render($content['field_recognized_uae']);?></h4>
-     <h4><?php print t('kwait: ').render($content['field_recognized_kw']);?></h4>
+  
      <table style="width:100%">
   <tr>
     <td><?php print t('Jordan')?></td>
