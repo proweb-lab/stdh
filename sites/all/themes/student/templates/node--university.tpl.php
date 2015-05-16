8c98de8492451c0bb0fe4837240e64a4ac56d4bf
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-16 14:55:06 +0300

    link module

--- sites/all/themes/student/templates/node--university.tpl.php
+++ sites/all/themes/student/templates/node--university.tpl.php
@@ -1,17 +1,21 @@
-<div class class="office-title">
+<div class="office-title">
+    <div class="label"><?php t('Country')?></div>
 <?php print render($content['field_office']); ?>
 </div>
-<div class="uni-specilty">
+<div class="uni-specialtiesy">
+    <div class="label"><?php t('Specialties')?></div>
     <?php print render($content['field_specialties']); ?>
 </div>
 <div class="uni-language">
+    <div class="label"><?php t('Study Language')?></div>
     <?php print render($content['field_language']); ?>
 </div>
 <div class="uni-recognition">
-    <div><?php print t('Jordan')?></div>
-     <div><?php print t('KSA')?></div>
-     <div><?php print t('UAE')?></div>
-     <div><?php print t('kwait')?></div>
+     <div class="label"><?php t('Univrsity Recognition ')?></div>
+    <div><?php print t('Jordan: ').render($content['field_recognized_jo']);?></div>
+     <div><?php print t('KSA: ').render($content['field_recognized_ksa']);?></div>
+     <div><?php print t('UAE: ').render($content['field_recognized_uae']);?></div>
+     <div><?php print t('kwait: ').render($content['field_recognized_kw']);?></div>
 </div>
 
 
