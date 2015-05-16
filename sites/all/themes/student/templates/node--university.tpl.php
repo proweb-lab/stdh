c547a2f83709d8f0d6dbc2691442cada987b1ba4
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-16 15:28:28 +0300

    link module

--- sites/all/themes/student/templates/node--university.tpl.php
+++ sites/all/themes/student/templates/node--university.tpl.php
@@ -1,18 +1,18 @@
 <div class="university-node">
 <div class="uni-office-title">
-    <h3 class="label"><?php print t('Country')?></h3>
+    <h3><?php print t('Country')?></h3>
 <?php print render($content['field_office']); ?>
 </div>
 <div class="uni-specialtiesy">
-    <h3 class="label"><?php print t('Specialties')?></h3>
+    <h3><?php print t('Specialties')?></h3>
     <?php print render($content['field_specialties']); ?>
 </div>
 <div class="uni-language">
-    <h3 class="label"><?php print t('Study Language')?></h3>
+    <h3><?php print t('Study Language')?></h3>
     <?php print render($content['field_language']); ?>
 </div>
 <div class="uni-recognition">
-     <h3 class="label"><?php print t('Univrsity Recognition ')?></h3>
+     <h3><?php print t('Univrsity Recognition ')?></h3>
     <h4><?php print t('Jordan: ').render($content['field_recognized_jo']);?></h4>
      <h4><?php print t('KSA: ').render($content['field_recognized_ksa']);?></h4>
      <h4><?php print t('UAE: ').render($content['field_recognized_uae']);?></h4>
