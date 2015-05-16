2b89f04677b1dc762898c2287ce4a1caf19a15b3
proweb-lab <mohammadgholeh@gmail.com>
2015-05-16 16:38:02 +0300

    create template node 69

--- sites/all/themes/student/templates/node--service.tpl.php
+++ sites/all/themes/student/templates/node--service.tpl.php
@@ -1,4 +1,4 @@
-<div class="service-block service-<?php print render($content['field_weight']);?> ">
+<div class="service-block">
 <h3 class="service-title">
         <?php print render($content['title']);?>
     </h3>
