3294e882bcccea5f08197c6b063834bcbd423834
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-16 11:48:25 +0300

    link module

--- sites/all/themes/student/templates/views/office/views-view-fields--office--front-block.tpl.php
+++ sites/all/themes/student/templates/views/office/views-view-fields--office--front-block.tpl.php
@@ -1,5 +1,6 @@
-<div class="office-block">
-   <?php
+<?php
+$html = '<div class="office-block">';
+
    $extra_title = $fields['field_extra_title']->content;
    if(!empty( $extra_title)){
     ?>
@@ -15,4 +16,11 @@
     <h3 class="office-title">
         <?php print $fields['title']->content; ?>
     </h3>
-</div>
+<?php
+$html .= '</div>';
+?>
+
+
+<?php
+echo l($html, $fields['field_link']->raw, array('html' => TRUE));
+?>
