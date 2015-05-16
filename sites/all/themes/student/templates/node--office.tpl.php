aeb754238ccc10122d0e0dc27a125c6d95676ef3
proweb-lab <mohammadgholeh@gmail.com>
2015-05-16 15:09:02 +0300

    create template node office 9

--- sites/all/themes/student/templates/node--office.tpl.php
+++ sites/all/themes/student/templates/node--office.tpl.php
@@ -1,3 +1,16 @@
-<div class="office-title">
-    <?php print render($title); ?>
+<div id="office">
+    <div class="office-title">
+        <h1 class="container">
+            <?php print $fields['title']->content; ?>
+        </h1>
+    </div>
+    <div class="office-image">
+        <?php print $fields['field_image']->content; ?>
+    </div>
+    <div class="office-information">
+        <?php print $fields['field_information']->content; ?>
+    </div>
+    <div class="office-map">
+        <?php print $fields['field_map']->content; ?>
+    </div>
 </div>
