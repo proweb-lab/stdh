3a73fe7f5e4ef3f9ce5cc9a6fa9a1ecb07570a68
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-16 11:54:37 +0300

    link module

--- sites/all/themes/student/templates/views/office/views-view-fields--office--front-block.tpl.php
+++ sites/all/themes/student/templates/views/office/views-view-fields--office--front-block.tpl.php
@@ -3,24 +3,23 @@ $html = '<div class="office-block">';
 
    $extra_title = $fields['field_extra_title']->content;
    if(!empty( $extra_title)){
-    ?>
+    
 
- <div class="ribbon-wrapper-green">
-     <span><?php print t('soon')?></span>
-   </div>
+ $html .= '<div class="ribbon-wrapper-green">';
+ $html .='<span>'.print t('soon') .'</span>';
+ $html .='</div>';
 
-    <?php }?>
-    <div class="office-image">
-        <?php print $fields['field_image']->content;?>
-    </div>
-    <h3 class="office-title">
-        <?php print $fields['title']->content; ?>
-    </h3>
-<?php
+   }
+    $html .='<div class="office-image">';
+       print $fields['field_image']->content;
+    $html .='</div>';
+    $html .='<h3 class="office-title">';
+      print $fields['title']->content; 
+    $html .='</h3>';
+
 $html .= '</div>';
-?>
 
 
-<?php
+
+
 echo l($html, $fields['field_link']->content, array('html' => TRUE));
-?>
