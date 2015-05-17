467a630e6971e5cee83029e4974ee920d3c97d63
proweb-lab <mohammadgholeh@gmail.com>
2015-05-17 15:06:58 +0300

    create new node other services

--- sites/all/themes/student/templates/node--77.tpl.php
+++ sites/all/themes/student/templates/node--77.tpl.php
@@ -10,7 +10,7 @@
                     <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>تامين قبول الجامعي($25)</label><br/>
                     <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>محاضرة تعريفية في تركيا ($25)</label><br/>
                     <br/>
-                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>($25)YOS, TOFEL, ILTES  التحضير لإمتحان   </label><br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>($25)التحضير لإمتحان YOS, TOFEL, ILTES  التحضير لإمتحان   </label><br/>
                     <br/>
                     <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>اصدار الاقامة($25)</label><br/>
                     <br/>
