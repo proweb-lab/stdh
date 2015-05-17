1301ed0cd14b56eec49750554dd1a315c1a95abc
proweb-lab <mohammadgholeh@gmail.com>
2015-05-17 15:03:21 +0300

    create new node other services

--- sites/all/themes/student/templates/node--77.tpl.php
+++ sites/all/themes/student/templates/node--77.tpl.php
@@ -6,11 +6,20 @@
                     <legend>خدمات متفرقة</legend>
                     <label >قائمة الخدمات</label>
                     <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>الاستقبال في المطار ($25)</label><br/>
-                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>تامين السكن ($25)</label><br/>
-                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>تامين قبول ($25)</label><br/>
-                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>تامينرخصة قيادة ($25)</label><br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>تامين السكن الداخلي ($25)</label><br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>تامين قبول الجامعي($25)</label><br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>محاضرة تعريفية في تركيا ($25)</label><br/>
                     <br/>
-
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>($25)YOS, TOFEL, ILTES  التحضير لإمتحان   </label><br/>
+                    <br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>اصدار الاقامة($25)</label><br/>
+                    <br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>تأمين السكن خارجي($25)</label><br/>
+                    <br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>تدريب على اللغة التركية($25)</label><br/>
+                    <br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>رحلات تعريفية بالتركيا بخصم 70%($25)</label><br/>
+                    <br/>
                     <div id="totalPrice"> Total: <input type="text" size="2" name="total" value="0"/></div>
 
                 </fieldset>
