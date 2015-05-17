a563a67b7f66679d1a2ad2fc4deec8b020bb98b1
proweb-lab <mohammadgholeh@gmail.com>
2015-05-17 14:37:12 +0300

    create new node other services

--- sites/all/themes/student/js/script.js
+++ sites/all/themes/student/js/script.js
@@ -32,14 +32,15 @@
 
 
        };
-    function checkTotal() {
-        document.listForm.total.value = '';
-        var sum = 0;
-        for (i=0;i<document.listForm.choice.length;i++) {
-            if (document.listForm.choice[i].checked) {
-                sum = sum + parseInt(document.listForm.choice[i].value);
-            }
-        }
-        document.listForm.total.value = sum;
-    }
+
 })(jQuery, Drupal, this, this.document);
+function checkTotal() {
+    document.listForm.total.value = '';
+    var sum = 0;
+    for (i=0;i<document.listForm.choice.length;i++) {
+        if (document.listForm.choice[i].checked) {
+            sum = sum + parseInt(document.listForm.choice[i].value);
+        }
+    }
+    document.listForm.total.value = sum;
+}
