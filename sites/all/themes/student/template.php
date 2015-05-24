8955f0006280e6a640bbfb10feb4eb1cb94345eb
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-24 16:59:39 +0300

    responsive menu

--- sites/all/themes/student/template.php
+++ sites/all/themes/student/template.php
@@ -8,5 +8,18 @@ function stdh_theme($existing, $type, $theme, $path){
     ),
   );
 }
+  
+function stdh_preprocess_html(&$variables, $hook) {
+  drupal_add_js('http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js', 'external');
+  
+  // Add variables and paths needed for HTML5 and responsive support.
+  $variables['base_path'] = base_path();
+
+  // Attributes for html element.
+  $variables['html_attributes_array'] = array(
+    'lang' => $variables['language']->language,
+    'dir' => $variables['language']->dir,
+  );
+}
 
 ?>
