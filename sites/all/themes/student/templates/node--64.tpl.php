de9abf0346159eb1428b27cc0f61c93d89be9907
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-24 12:10:06 +0300

    revert changes 24/5

--- sites/all/themes/student/templates/node--64.tpl.php
+++ sites/all/themes/student/templates/node--64.tpl.php
@@ -1,12 +1,12 @@
 <div class="row">
-    <div class="col-sm-4 pull-left" >
+    <div class="col-sm-4 pull-right" >
         <div class="contact_info"> <div class="addresses_header"><h4><?php print t('Addresses information'); ?></h4></div>
             <h5 class="phone_label"><?php print t("phone:") . render($content['field_phone']); ?></h5>
             <h5 class="email_label"><?php print t("email:"); ?><a href="mailto:info@studenthome.com">info@studenthome.com</a></h5>
 
 
         </div>
-        <div class="col-sm-8 pull-right">
+        <div class="col-sm-8 pull-left">
             <?php
             $block_contact = module_invoke('webform', 'block_view', 'client-block-64');
             print render($block_contact['content']);
