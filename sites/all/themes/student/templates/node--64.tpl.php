34ef8f1374fa910de2baa4310cf8433d54f72ede
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-24 11:03:16 +0300

    payment module

--- sites/all/themes/student/templates/node--64.tpl.php
+++ sites/all/themes/student/templates/node--64.tpl.php
@@ -1,16 +1,16 @@
 <div class="row">
-    <div class="col-sm-8">
-<?php
-   $block_contact = module_invoke('webform', 'block_view', 'client-block-64');
-   print render($block_contact['content']);
-?>
-        </div>
     <div class="col-sm-4" >
         <div class="contact_info"> <div class="addresses_header"><h4><?php print t('Addresses information'); ?></h4></div>
-        <h5 class="phone_label"><?php print t("phone:").render($content['field_phone']);?></h5>
-            <h5 class="email_label"><?php print t("email:");?><a href="mailto:info@studenthome.com">info@studenthome.com</a></h5>
-    
+            <h5 class="phone_label"><?php print t("phone:") . render($content['field_phone']); ?></h5>
+            <h5 class="email_label"><?php print t("email:"); ?><a href="mailto:info@studenthome.com">info@studenthome.com</a></h5>
 
+
+        </div>
+        <div class="col-sm-8">
+            <?php
+            $block_contact = module_invoke('webform', 'block_view', 'client-block-64');
+            print render($block_contact['content']);
+            ?>
+        </div>
     </div>
- </div>
 </div>
