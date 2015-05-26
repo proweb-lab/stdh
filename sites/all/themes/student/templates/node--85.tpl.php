c56187f78c96a8fb834cee9130da88173a11ca7c
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-26 11:27:52 +0300

    modernizr js

---
+++ sites/all/themes/student/templates/node--85.tpl.php
@@ -0,0 +1,26 @@
+<article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <div class="apply">
+    <?php
+    global $user;
+
+    $form_state = array();
+    form_load_include($form_state, "inc", "node", "node.pages");
+    $application = new stdClass();
+    $application->type = 'application';
+    $application->uid = $user->uid;
+    $application->name = (isset($user->name) ? $user->name : '');
+    // Hidden through form alter
+    $application->field_app_position['und'][0]['nid'] = $node->nid;
+    node_object_prepare( $application );
+
+    echo drupal_render(drupal_get_form( 'application_node_form', $application ));
+    ?>
+    <?php //print render(node_view(node_load(1), 'full', NULL)); ?>
+  </div>
+</div>
+</article>
+<?php
+//  $block = module_invoke('menu_block', 'block_view', 2);
+// print render($block['content']);
+?>
