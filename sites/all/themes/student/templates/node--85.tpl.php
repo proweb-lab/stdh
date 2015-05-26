280f8d8e9a249fca04004efa4c302180d4f32b91
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-26 11:49:36 +0300

    modernizr js

--- sites/all/themes/student/templates/node--85.tpl.php
+++ sites/all/themes/student/templates/node--85.tpl.php
@@ -9,7 +9,7 @@
     $application = new stdClass();
     $application->type = 'application';
     $application->uid = $user->uid;
-    $application->name = (isset($user->name) ? $user->name : '');
+    
     // Hidden through form alter
     $application->field_app_position['und'][0]['nid'] = $node->nid;
     node_object_prepare( $application );
