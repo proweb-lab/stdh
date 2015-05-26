c9eaeb648276391bb6713cb43841019922d419ad
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-05-26 14:40:37 +0300

    modernizr js

--- sites/all/themes/student/templates/node--85.tpl.php
+++ sites/all/themes/student/templates/node--85.tpl.php
@@ -11,9 +11,9 @@
         $application->uid = $user->uid;
         $application->name = (isset($user->name) ? $user->name : '');
         print_r($application->name);
-        $application->field_full_name['und'][0]['nid']= $application->name;
+        $application->field_full_name['und'][0]['uid']= $user->uid;
         // Hidden through form alter
-        //$application->field_app_position['und'][0]['nid'] = $node->nid;
+        //$application->field_app_position['und'][0]['nid'] = $user->uid$node->nid;
         node_object_prepare($application);
         $application_form = drupal_get_form('application_node_form', $application);
         $application_variable = drupal_render($application_form);
