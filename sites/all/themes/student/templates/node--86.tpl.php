50705364d7e3369058a64cbe8f2e09ba6ddd54f7
proweb-lab <mohammadgholeh@gmail.com>
2015-05-30 12:07:11 +0300

    test other services

--- sites/all/themes/student/templates/node--86.tpl.php
+++ sites/all/themes/student/templates/node--86.tpl.php
@@ -1,9 +1,4 @@
 <?php
-$form['expire_date'] = array(
-'#type' => 'checkbox',
-'#title' => t('Set Expiration Date for this Group'),
-'#description' => t('Change this date to set Expiration of Role.'),
-'#default_value' => $date_default_value,
-);
+
 
 ?>
