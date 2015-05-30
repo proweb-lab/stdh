735ac80b878e5d23830e8840d97f7f40ff654d70
proweb-lab <mohammadgholeh@gmail.com>
2015-05-30 11:15:32 +0300

    test other services

--- sites/all/themes/student/templates/node--86.tpl.php
+++
@@ -1,5 +0,0 @@
-<div class="form-type-checkbox form-item-submitted-serivces-airport form-item checkbox">
-    <input type="checkbox" id="edit-submitted-serivces-1" name="submitted[airport]" value="airport" class="form-checkbox">  <label for="edit-submitted-serivces-1">استقبال في المطار </label>
-    <input type="checkbox" id="edit-submitted-serivces-1" name="submitted[serivces]" value="serivces" class="form-checkbox">  <label for="edit-submitted-serivces-1">استقبال في المطار </label>
-
-</div>
