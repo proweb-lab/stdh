0e1a1f437df8a9b3c3f3f579d10f3acf42192b10
fadi <fadi@mfsaadedin>
2015-08-07 18:49:01 +0300

    menu  link css

--- sites/all/themes/student/templates/node--85.tpl.php
+++ sites/all/themes/student/templates/node--85.tpl.php
@@ -4,20 +4,23 @@
         <div class="col-sm-6">
         <?php
         global $user;
-        print getNew_form('application'); 
-//<!--     $form_state = array();
-//      form_load_include($form_state, "inc", "node", "node.pages");
-//        $application = new stdClass();
-//        $application->type = 'application';
-//        $application->uid = $user->uid;
-//        $application->name = (isset($user->name) ? $user->name : '');
-//        $application->field_full_name= $user->name;
-//        // Hidden through form alter
-//        //$application->field_app_position['und'][0]['nid'] = $user->uid$node->nid;
-//        node_object_prepare($application);
-//        $application_form = drupal_get_form('application_node_form', $application);
-//        $application_variable = drupal_render($application_form);
-//        echo $application_variable;
+       // print getNew_form('application'); 
+    global $user;
+
+        $form_state = array();
+        form_load_include($form_state, "inc", "node", "node.pages");
+        $application = new stdClass();
+        $application->type = 'application';
+        $application->uid = $user->uid;
+        $application->name = (isset($user->name) ? $user->name : '');
+    
+        //$application->field_full_name= $user->name;
+        // Hidden through form alter
+        //$application->field_app_position['und'][0]['nid'] = $user->uid$node->nid;
+        node_object_prepare($applications);
+        $applications_form = drupal_get_form('application_node_form', $applications);
+        $applications_variable = drupal_render(node_add($applications_form));
+        echo $applications_variable;
         
 //          $form_state = array();
 //  form_load_include($form_state, "inc", "node", "node.pages");
