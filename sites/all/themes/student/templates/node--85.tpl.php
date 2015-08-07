e0b68cd7e9aebbced2753dccb13f2c4e6d716af6
fadi <fadi@mfsaadedin>
2015-08-07 19:06:14 +0300

    menu  link css

--- sites/all/themes/student/templates/node--85.tpl.php
+++ sites/all/themes/student/templates/node--85.tpl.php
@@ -7,27 +7,27 @@
        // print getNew_form('application'); 
     global $user;
 
-//        $form_state = array();
-//        form_load_include($form_state, "inc", "node", "node.pages");
-//        $application = new stdClass();
-//        $application->type = 'application';
-//        $application->uid = $user->uid;
-//        $application->name = (isset($user->name) ? $user->name : '');
-//    
-//        //$application->field_full_name= $user->name;
-//        // Hidden through form alter
-//        //$application->field_app_position['und'][0]['nid'] = $user->uid$node->nid;
-//        node_object_prepare($application);
-//        $applications_form = drupal_get_form('application_node_form', $application);
-//        $applications_variable = drupal_render($applications_form);
-//        echo $applications_variable;
-        
+        $form_state = array();
+        form_load_include($form_state, "inc", "node", "node.pages");
+        $application = new stdClass();
+        $application->type = 'application';
+        $application->uid = $user->uid;
+        $application->name = (isset($user->name) ? $user->name : '');
     
-    if (!function_exists('node_add')) {
-  module_load_include('inc', 'node', 'node.pages');
-}
- $from = node_add('application');
-print drupal_render($form);
+        //$application->field_full_name= $user->name;
+        // Hidden through form alter
+        //$application->field_app_position['und'][0]['nid'] = $user->uid$node->nid;
+        node_object_prepare($application);
+        $applications_form = drupal_get_form('application_node_form', $application);
+        $applications_variable = drupal_render($applications_form);
+        echo $applications_variable;
+        
+////    
+////    if (!function_exists('node_add')) {
+////  module_load_include('inc', 'node', 'node.pages');
+//}
+// $from = node_add('application');
+//print drupal_render($form);
 
 //          $form_state = array();
 //  form_load_include($form_state, "inc", "node", "node.pages");
