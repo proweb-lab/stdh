f8b8235d5ad6452a9d1c31fa1105ca58d59ed045
Mohammad Gholeh <mohammad@redtroops.com>
2015-08-29 01:35:49 +0300

    node 78  add all  services and pricing

--- sites/all/themes/student/templates/node--78.tpl.php
+++ sites/all/themes/student/templates/node--78.tpl.php
@@ -11,12 +11,13 @@
                     <label class='radiolabel'><input type="checkbox" name="choice" value="42.74" onchange="checkTotal()"/>تأمين السكن الداخلي في الجامعة ($42.74)</label><br/>
                     <label class='radiolabel'><input type="checkbox" name="choice" value="71.23" onchange="checkTotal()"/>محاضرة تعريفية عن دولة الدراسة في دولة الدراسة ($71.23)</label><br/>
                     <label class='radiolabel'><input type="checkbox" name="choice" value="28.49" onchange="checkTotal()"/>المساعدة في التحضير لامتحانات القبول الجامعي من خلال إرشادهم إلى المراكز المعنية($28.49)</label><br/>
-                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>تأمين السكن خارجي($25)</label><br/>
-                    <br/>
-                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>تدريب على اللغة التركية($25)</label><br/>
-                    <br/>
-                    <label class='radiolabel'><input type="checkbox" name="choice" value="25" onchange="checkTotal()"/>رحلات تعريفية بالتركيا بخصم 70%($25)</label><br/>
-                    <br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="213.68" onchange="checkTotal()"/>تأمين السكن خارجي($213.68)</label><br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="142.45" onchange="checkTotal()"/> المساعدة في إصدار الإقامة($142.45)</label><br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="99.72" onchange="checkTotal()"/>جولة سياحية ($99.72)</label><br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="213.68" onchange="checkTotal()"/>استكمال إجراءات التسجيل ($213.68)</label><br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="356.13" onchange="checkTotal()"/>دورة أساسية في اللغة التركية تعقد في تركيا ($356.13)</label><br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="170.94" onchange="checkTotal()"/>المساعدة في استخراج الرقم الضريبي وفتح الحساب البنكي  ($170.94)</label><br/>
+                    <label class='radiolabel'><input type="checkbox" name="choice" value="142.45" onchange="checkTotal()"/>رحلة ترفيهية وتعليمية خلال السنة الدراسية للطلبة  ($142.45)</label><br/>
                     <div id="totalPrice"> السعر: <input type="text" size="2" id="txtResult"name="total" readonly="readonly" value="0"/></div>
 
                 </fieldset>
