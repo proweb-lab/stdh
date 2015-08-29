48db7997eb5dd70f68d45beccf41442e02367f32
fadi <fadi@mfsaadedin>
2015-08-29 17:39:49 +0300

    new modules

--- sites/all/themes/student/js/script.js
+++ sites/all/themes/student/js/script.js
@@ -7,10 +7,10 @@
     $(this).next('.colloction-info').slideToggle("slow");
     $(this).toggleClass("active");
   });
-          $('.payment-line-item-amount.form-control').val('100');
-          $('#edit-field-pay-und-line-item-container-0-quantity').val('1');
-          $('#edit-field-pay-und-line-item-container-0-tax-rate').val('1');
-          $('#edit-field-pay-und-line-item-container-0-description').val('Service fees');
+//          $('.payment-line-item-amount.form-control').val('100');
+//          $('#edit-field-pay-und-line-item-container-0-quantity').val('1');
+//          $('#edit-field-pay-und-line-item-container-0-tax-rate').val('1');
+//          $('#edit-field-pay-und-line-item-container-0-description').val('Service fees');
           
           
 
