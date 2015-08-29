7dc65972e9f30a16406730ac80ed814f01c1ba2a
fadi <fadi@mfsaadedin>
2015-08-29 12:43:58 +0300

    new modules

--- sites/all/themes/student/templates/node--236.tpl.php
+++ sites/all/themes/student/templates/node--236.tpl.php
@@ -6,27 +6,27 @@
         <?php
         global $user;
 
-//        $form_state = array();
-//        form_load_include($form_state, "inc", "node", "node.pages");
-//        $application = new stdClass();
-//        $application->type = 'fixed_payment';te = array();
-//        form_load_include($form_state, "inc", "node", "node.pages
-//        $application->uid = $user->uid;
-//        $application->name = (isset($user->name) ? $user->name : '');
-//    
-//        $application->field_full_name= $user->name;
-//        // Hidden through form alter
-//        //$application->field_app_position['und'][0]['nid'] = $user->uid$node->nid;
-//        node_object_prepare($application);
-//        $application_form = drupal_get_form('fixed_payment_node_form', $application);
-//        $application_variable = drupal_render($application_form);
-//        echo $application_variable;
+        $form_state = array();
+        form_load_include($form_state, "inc", "node", "node.pages");
+        $application = new stdClass();
+        $application->type = 'fixed_payment';
+        form_load_include($form_state, "inc", "node", "node.pages");
+        $application->uid = $user->uid;
+        $application->name = (isset($user->name) ? $user->name : '');
+    
+        $application->field_full_name= $user->name;
+        // Hidden through form alter
+        //$application->field_app_position['und'][0]['nid'] = $user->uid$node->nid;
+        node_object_prepare($application);
+        $application_form = drupal_get_form('fixed_payment_node_form', $application);
+        $application_variable = drupal_render($application_form);
+        echo $application_variable;
         
 //              $form_state = array();
 //       form_load_include($form_state, "inc", "node", "node.pages");
 //          module_load_include('inc', 'node', 'node.pages');    
-  $form = drupal_get_form("stdh_fixed_payment_form");
-  echo $rtn = drupal_render($form);
+//  $form = drupal_get_form("stdh_fixed_payment_form");
+//  echo $rtn = drupal_render($form);
         
         
         ?>
