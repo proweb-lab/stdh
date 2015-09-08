d95146c019ed36da68b6113fac85ee31dbfb5430
fadi <fadi@mfsaadedin>
2015-09-09 00:06:16 +0300

    location icon size

--- sites/all/themes/student/templates/views/universities/views-view-fields--universities--page.tpl.php
+++ sites/all/themes/student/templates/views/universities/views-view-fields--universities--page.tpl.php
@@ -2,7 +2,7 @@
 
 <?php
 
-            if($fields['field_recognized_jo']->content=='yes'){
+            if($fields['field_recognized_jodan']->content=='yes'){
             echo '<span class="reg-country yes">';
          
             print '</span>';
