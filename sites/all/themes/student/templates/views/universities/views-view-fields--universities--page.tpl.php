38d819cbe633d91b5b5aa77d22d57c52e2d6a989
fadi <fadi@mfsaadedin>
2015-09-09 00:02:33 +0300

    location icon size

--- sites/all/themes/student/templates/views/universities/views-view-fields--universities--page.tpl.php
+++ sites/all/themes/student/templates/views/universities/views-view-fields--universities--page.tpl.php
@@ -1,26 +1,19 @@
 <span class="uni-name list" ><?php print $fields['title']->content; ?></span>
 
 <?php
-//$country = intval($node->field_recognition['und'][0]['value']);
-// switch($country){
-//print '<pre>';
-//print_r($fields['field_recognized_jo']);
-//print '</pre>';
-//$jo = field_info_field('field_recognized_jo');
-//$allowed_values_jo = list_allowed_values($field);
-//print_r($allowed_values[2]);
 
-         if($fields['field_recognized_jo']->content=='no'){
-            echo '<span class="reg-country no">';
+            if($fields['field_recognized_jo']->content=='yes'){
+            echo '<span class="reg-country yes">';
          
             print '</span>';
-         }
+         } 
+         
         
-       else{
-             echo '<span class="reg-country yes">';
-           
-           print '</span>';
-        }
+         else{
+              echo '<span class="reg-country no">';
+        
+            print '</span>';
+         }
        
          if($fields['field_recognized_ksa']->content=='Yes'){
             echo '<span class="reg-country yes">';
@@ -59,9 +52,7 @@
             print '</span>';
          }
     
-       
-   
- 
+      
        ?>
 <!--$field = field_info_field('field_name');
 $allowed_values = list_allowed_values($field);-->
