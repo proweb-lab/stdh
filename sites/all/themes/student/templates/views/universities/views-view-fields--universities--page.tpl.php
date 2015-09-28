908cf507ecff4f79b4a29a3735c1b7894ac3083b
Fadi <mf.saadeldin@gmail.com>
2015-09-28 21:13:30 +0300

    node product

--- sites/all/themes/student/templates/views/universities/views-view-fields--universities--page.tpl.php
+++ sites/all/themes/student/templates/views/universities/views-view-fields--universities--page.tpl.php
@@ -2,8 +2,7 @@
 
 <?php
 
-    print_r($fields['field_recognized_jo']->content);
-       
+   
          if($fields['field_recognized_ksa']->content=='Yes'){
             echo '<span class="reg-country yes">';
         
@@ -13,6 +12,17 @@
          else{
               echo '<span class="reg-country no">';
        
+            print '</span>';
+         }
+                  if($fields['field_recognized_jo']->content==='No'){
+            echo '<span class="reg-country no">';
+         
+            print '</span>';
+         } 
+        
+         else{
+              echo '<span class="reg-country yes">';
+        
             print '</span>';
          }
   
@@ -41,17 +51,7 @@
         
             print '</span>';
          }
-               if($fields['field_recognized_jo']->content==='No'){
-            echo '<span class="reg-country no">';
-         
-            print '</span>';
-         } 
-        
-         else{
-              echo '<span class="reg-country yes">';
-        
-            print '</span>';
-         }
+      
     
       
        ?>
